a7909c7321e65f2abc8d48f9dc86a6f2e22f3fd3
Komputer <Komputer@DESKTOP-M3MVE9B>
2023-04-19 19:38:43 +0200

    Dodaj pliki projektów.

---
+++ Paper-soccer/BoardFrame.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <wx/wx.h>
+using namespace std;
+class BoardFrame : public wxFrame
+{	
+private:
+	int x = 7;
+	int y = 11;
+	
+public:
+	int current_x = 20;
+	int current_y = 20;
+	BoardFrame(const wxString& title);
+	void drawLine(wxCommandEvent& event, int* x1, int* y1, int x2, int y2);
+};
+
