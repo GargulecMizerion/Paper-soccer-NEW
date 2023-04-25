008c42b242ea89ec0f21fb152c213d3eb96045cd
Komputer <Komputer@DESKTOP-M3MVE9B>
2023-04-25 18:31:59 +0200

    siemanko 3

--- Paper-soccer/App.cpp
+++ Paper-soccer/App.cpp
@@ -1,13 +1,21 @@
 #include "App.h"
 #include <wx/wx.h>
 #include "BoardFrame.h"
+#include "StartFrame.h"
 
 using namespace std;
 wxIMPLEMENT_APP(App);
 
 bool App::OnInit() {
 	BoardFrame* boardFrame = new BoardFrame("Paper Soccer");
+	StartFrame* startFrame = new StartFrame("Paper Soccer");
+
+
 	boardFrame->SetClientSize(400, 500);
-	boardFrame->Show();
+	startFrame->SetClientSize(400, 500);
+
+	startFrame->Show();
+	//boardFrame->Show();
+
 	return true;
 }
